5f960bb2d9a050d37e642d939394a4c94131c2b3
zugzug <lumber.bug@gmail.com>
2016-03-05 11:02:36 -0800

    braindead qa: pass the buck to ruby
    This commit adds brain-dead support for 'cd basedir && qa' in directories that
    look like:
    
    basedir/
      lib/
        foo.rb
      test/
        test-foo.rb
    
    TODO
    - test harness for qa itself
    - actual language detection

---
+++ qa.go
@@ -0,0 +1,52 @@
+package main
+// cd <basedir> && qa
+
+import (
+  "fmt"
+  "os"
+  "os/exec"
+  "syscall"
+)
+
+// TODO which ruby version must qa want to run?
+func assert_must_have_ruby() {
+  if err := exec.Command("which", "ruby").Run(); err != nil {
+    fmt.Fprintf(os.Stderr, "You don't have ruby on your system\n")
+    os.Exit(1)
+  }
+}
+
+// Assume this directory structure
+//
+// basedir/
+//  lib/
+//    foo.rb
+//    ...
+//  test/
+//    test-foo.rb
+//    test-*.rb
+//    ...
+func detect_and_run_ruby_tests() {
+  assert_must_have_ruby()
+  args := []string{"-I", "lib", "-e",
+`tests = Dir.glob('./test/test[-_]*.rb').to_a
+abort('QA: No tests found!') if tests.empty?
+
+puts "QA: Found #{tests.size} test files"
+require 'minitest/autorun'
+tests.each {|t| require(t) }`}
+  cmd := exec.Command("ruby", args...)
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+
+  if err := cmd.Run(); err != nil {
+    if exitError, ok := err.(*exec.ExitError); ok {
+      waitStatus := exitError.Sys().(syscall.WaitStatus)
+      os.Exit(waitStatus.ExitStatus())
+    }
+  }
+}
+
+func main() {
+  detect_and_run_ruby_tests()
+}
